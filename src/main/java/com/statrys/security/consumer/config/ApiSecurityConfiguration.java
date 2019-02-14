cbff7e03084698395517b685661ddf70db01e3f1
Andrew Chan <andrew.koonkit.chan@gmail.com>
2019-02-14 17:06:57 +0800

    PMT-279 add config

--- src/main/java/com/statrys/security/consumer/config/ApiSecurityConfiguration.java
+++ src/main/java/com/statrys/security/consumer/config/ApiSecurityConfiguration.java
@@ -1,8 +1,24 @@
 package com.statrys.security.consumer.config;
 
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 
 @EnableWebSecurity
 public class ApiSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.cors().and()
+                .csrf().disable()
+                .authorizeRequests()
+                .mvcMatchers( "/health", "/credentials").permitAll()
+                .mvcMatchers(HttpMethod.POST, "/password").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+        ;
+    }
 }
