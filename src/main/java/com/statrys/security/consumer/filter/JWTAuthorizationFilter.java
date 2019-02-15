d55e7c544419bdabfc8efb91c2fd129d269a28eb
Andrew Chan <andrew.koonkit.chan@gmail.com>
2019-02-15 16:40:40 +0800

    PMT-279 make sure throw spring standard security exception when token cannot be parsed

--- src/main/java/com/statrys/security/consumer/filter/JWTAuthorizationFilter.java
+++ src/main/java/com/statrys/security/consumer/filter/JWTAuthorizationFilter.java
@@ -8,10 +8,12 @@ import com.nimbusds.jose.jwk.JWKSet;
 import com.nimbusds.jose.jwk.RSAKey;
 import com.statrys.security.consumer.constant.SecurityConstants;
 import com.statrys.security.consumer.model.WellKnownJsonUrl;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 import javax.servlet.FilterChain;
@@ -23,6 +25,7 @@ import java.net.URL;
 import java.security.interfaces.RSAPublicKey;
 import java.text.ParseException;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 import static com.auth0.jwt.algorithms.Algorithm.RSA256;
@@ -45,11 +48,11 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
             return;
         }
 
-        UsernamePasswordAuthenticationToken authentication = null;
+        UsernamePasswordAuthenticationToken authentication;
         try {
             authentication = getAuthentication(request);
         } catch (ParseException | JOSEException e) {
-            e.printStackTrace();
+            throw new AuthenticationCredentialsNotFoundException("Fail to get Authentication");
         }
 
         SecurityContextHolder.getContext().setAuthentication(authentication);
@@ -61,8 +64,8 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
         if (token != null) {
             // parse the token.
             JWKSet jwkSet = JWKSet.load(new URL(wellKnownJsonUrl.getUrl()));
-            if (jwkSet.getKeys().isEmpty() || !(jwkSet.getKeys().get(0) instanceof RSAKey)){
-                return null;
+            if (jwkSet.getKeys().isEmpty() || !(jwkSet.getKeys().get(0) instanceof RSAKey)) {
+                throw new AuthenticationCredentialsNotFoundException("Fail to get JWKS");
             }
             com.nimbusds.jose.jwk.RSAKey rsaKey = (com.nimbusds.jose.jwk.RSAKey) jwkSet.getKeys().get(0);
             RSAPublicKey rsaPublicKey = rsaKey.toRSAPublicKey();
@@ -71,14 +74,19 @@ public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
                     .build()
                     .verify(token.replace(SecurityConstants.TOKEN_PREFIX, ""));
             String principal = decodedJWT.getSubject();
-
-            if (principal != null) {
-                return new UsernamePasswordAuthenticationToken(principal, null, decodedJWT.getClaim(SecurityConstants.AUTHORITIES).asList(String.class)
-                        .stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList()));
+            List<SimpleGrantedAuthority> simpleGrantedAuthorities = null;
+            if (decodedJWT.getClaim(SecurityConstants.AUTHORITIES) != null) {
+                simpleGrantedAuthorities = decodedJWT.getClaim(SecurityConstants.AUTHORITIES).asList(String.class)
+                        .stream().map(SimpleGrantedAuthority::new).collect(Collectors.toList());
             }
-            return null;
+            if (principal != null) {
+                return new UsernamePasswordAuthenticationToken(principal, null, simpleGrantedAuthorities);
+            } else {
+                throw new AuthenticationCredentialsNotFoundException("Fail to parse the principal from the token");
+            }
+        } else {
+            throw new AuthenticationCredentialsNotFoundException("Fail to get the token header");
         }
-        return null;
     }
 }
 
