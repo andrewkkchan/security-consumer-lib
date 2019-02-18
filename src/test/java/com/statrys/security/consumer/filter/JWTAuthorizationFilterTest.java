989cf0ce254729c3c3a6f5b26ae49516d2b9326c
Andrew Chan <andrew.koonkit.chan@gmail.com>
2019-02-18 14:48:15 +0800

    PMT-279 add unit tests

---
+++ src/test/java/com/statrys/security/consumer/filter/JWTAuthorizationFilterTest.java
@@ -0,0 +1,16 @@
+package com.statrys.security.consumer.filter;
+
+
+import org.junit.Test;
+import org.mockito.MockitoAnnotations;
+
+public class JWTAuthorizationFilterTest {
+    public void before() {
+        MockitoAnnotations.initMocks(this);
+    }
+    @Test
+    public void testGetAuthentication(){
+
+    }
+
+}
