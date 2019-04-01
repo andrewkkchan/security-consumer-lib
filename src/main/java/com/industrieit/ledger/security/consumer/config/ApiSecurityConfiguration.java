8df30ecf697aa532d516d921cf9b4129b25d0712
Andrew Chan <andrew.koonkit.chan@gmail.com>
2019-04-01 11:50:00 +0800

    rescind graphql

--- src/main/java/com/industrieit/ledger/security/consumer/config/ApiSecurityConfiguration.java
+++ src/main/java/com/industrieit/ledger/security/consumer/config/ApiSecurityConfiguration.java
@@ -42,7 +42,7 @@ public class ApiSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .mvcMatchers( "/health", "/credentials").permitAll()
                 .antMatchers("/api-docs", "/configuration/ui", "/swagger-resources", "/configuration/security",
-                        "/swagger-ui.html", "/webjars/**","/swagger-resources/configuration/ui","/swagger-ui.html","/graphiql").permitAll()
+                        "/swagger-ui.html", "/webjars/**","/swagger-resources/configuration/ui","/swagger-ui.html").permitAll()
                 .mvcMatchers(HttpMethod.POST, "/user").permitAll()
                 .mvcMatchers(HttpMethod.POST, "/password").permitAll()
                 .mvcMatchers(HttpMethod.POST, "/password/recovery").permitAll()
